340e77258e4d9352bd57b5b14f8e5dc7c0699129
Maria <zhukovamaria9@gmail.com>
2023-12-17 23:45:27 +0300

    Add saving to local storage

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,8 @@ const tasksList = document.getElementById("tasks");
 const emptyParagraph = document.getElementById("empty");
 const clearButton = document.getElementById("clear");
 const allCheckboxes = document.querySelectorAll("checkbox");
+const tasksArray = [];
+const getTasks = localStorage.getItem("tasksArray");
 
 function createTask() {
   const newTask = document.createElement("li");
@@ -16,6 +18,8 @@ function createTask() {
   newTask.append(checkbox);
   input.value = "";
   clearButton.removeAttribute("disabled");
+  tasksArray.push(newTaskText);
+  localStorage.setItem("tasksArray", JSON.stringify(tasksArray));
 }
 
 function checkTask(evt) {
@@ -28,10 +32,7 @@ function clearList() {
   tasksList.textContent = "";
   emptyParagraph.textContent = "Nothing here yet";
   clearButton.setAttribute("disabled", "");
-}
-
-function showEmpty() {
-  emptyParagraph.textContent = "Nothing here yet";
+  localStorage.removeItem("tasksArray");
 }
 
 addButton.addEventListener("click", createTask);
@@ -40,4 +41,22 @@ document.addEventListener("keypress", function onEvent(event) {
   if (event.key === "Enter") createTask();
 });
 clearButton.addEventListener("click", clearList);
-document.addEventListener("DOMContentLoaded", showEmpty);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const parsedArray = JSON.parse(getTasks);
+  const newTask = document.createElement("li");
+  const checkbox = document.createElement("input");
+  if (getTasks) {
+    JSON.parse(getTasks);
+    clearButton.removeAttribute("disabled");
+    emptyParagraph.textContent = "";
+    for (let i = 0; i < parsedArray.length; i++) {
+      tasksList.append(newTask);
+      newTask.textContent = parsedArray[i];
+    }
+    for (let i = 0; i < parsedArray.length; i++) {
+      newTask.append(checkbox);
+      checkbox.type = "checkbox";
+    }
+  }
+});
